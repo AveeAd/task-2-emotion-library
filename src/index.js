f414e3f7f928a69bf68a752b9586e93b4527a40d
Kumar Adhikari / Avee <kumarad556@gmail.com>
2021-12-08 15:18:04 +0545

    removed basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/home">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
